dbb41f838e3ef7bd16099a7ceaa8e2c9b166a644
Satherov <63814891+Satherov@users.noreply.github.com>
2025-09-04 20:03:24 +0200

    YES I DO

--- kubejs/server_scripts/tweaks/allthecompressed.js
+++ kubejs/server_scripts/tweaks/allthecompressed.js
@@ -63,7 +63,7 @@ ServerEvents.recipes(allthemods => {
                     stack
                 ]
             }
-        ).id(`allthemods:create/ending/${id}`)
+        ).id(`allthemods:create_dragons_plus/ending/${id}`)
     }
 
     for (let i = 1; i < 10; i++) {
