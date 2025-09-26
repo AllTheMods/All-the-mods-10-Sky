8ec95fde0659fbdbda080081f1d96d0d774600ca
SrNadien 🔥 <76491773+SrNadien@users.noreply.github.com>
2025-09-25 22:07:08 -0300

    Update mystical_agriculture.js

--- kubejs/server_scripts/mystical_agriculture.js
+++ kubejs/server_scripts/mystical_agriculture.js
@@ -9,7 +9,7 @@ ServerEvents.recipes( nadien => {
             essenceCircle('allthemodium:unobtainium_ingot', 'unobtainium')     
             essenceCircle('hostilenetworks:prediction_matrix', 'predicciondematrix')
             essenceCircle('extendedcrafting:redstone_ingot', 'redstoneingot')
-            essenceCircle('avaritia:crystal_matrix_ingot', 'crystal_matrix')
+            // essenceCircle('avaritia:crystal_matrix_ingot', 'crystal_matrix')
             essenceCircle('industrialforegoing:pink_slime_ingot', 'pink_slime')
             essenceCircle('ars_nouveau:source_gem', 'source_gem')
             essenceCircle('immersiveengineering:plate_duroplast', 'duroplast')
@@ -17,7 +17,7 @@ ServerEvents.recipes( nadien => {
             essenceCircle('draconicevolution:chaos_shard' , 'chaos_shard')  
             essenceCircle('create:andesite_alloy' , 'andesite_alloy')
             essenceCircle('megacells:sky_steel_ingot', 'sky_steel_ingot')
-            essenceCircle('mekanism_extras:ingot_naquadah', 'naquadah')
+            // essenceCircle('mekanism_extras:ingot_naquadah', 'naquadah')
             essenceCircle('occultism:spirit_attuned_gem', 'spirit_gem')
             essenceCircle('mysticalagriculture:prosperity_shard', 'prosperity_shard')
             essenceCircle('create:polished_rose_quartz', 'cuarzorosa')
@@ -42,48 +42,5 @@ ServerEvents.recipes( nadien => {
     //remove nitro crystal crux
     nadien.remove({ id: 'mysticalagradditions:nitro_crystal_crux' })
 
- //ornium essence
- nadien.replaceInput({id: 'mysticalextendedtier:ornium_essence'}, 'mysticalagriculture:supremium_essence', 'mysticalagradditions:insanium_essence')
-
-function addInfustion(seed, item, essence) {
-        nadien.custom(
-            {
-                type: 'mysticalagriculture:infusion',
-                input: {
-                    item:  'mysticalagriculture:prosperity_seed_base'
-                },
-                ingredients: [
-                    {
-                        item: item
-                    },
-                    {
-                        item: essence
-                    },
-                    {
-                        item: item
-                    },
-                    {
-                        item: essence
-                    },
-                    {
-                        item: item
-                    },
-                    {
-                        item: essence
-                    },
-                    {
-                        item: item
-                    },
-                    {
-                        item: essence
-                    }
-                ],
-                result: {
-                    id: seed
-                }
-            }
-        ).id(seed.replace(':', ':infusion/'))}
-
-      addInfustion('mysticalagriculture:draconium_seeds', 'draconicevolution:draconium_block', 'mysticalagriculture:supremium_essence') 
 
 })
