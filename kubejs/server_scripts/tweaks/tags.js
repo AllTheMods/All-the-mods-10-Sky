d1711e709c603dccc1885edd3bfcc9f67209eb7d
DivineFinal <24657715+DivineFinal@users.noreply.github.com>
2025-12-09 04:20:57 +0000

    chore: prettier formatting

--- kubejs/server_scripts/tweaks/tags.js
+++ kubejs/server_scripts/tweaks/tags.js
@@ -49,7 +49,7 @@ ServerEvents.tags("block", (allthemods) => {
   allthemods.add("justdirethings:tick_speed_deny", "#allthemods:tick_acceleration_blacklist")
   allthemods.add("tiab:un_acceleratable", "#allthemods:tick_acceleration_blacklist")
   //allthemods.add('industrialforegoingsouls:cant_accelerate', '#allthemods:tick_acceleration_blacklist')
-  
+
   // Fix Jank with Feral Flares and Enchanting
   allthemods.add("minecraft:replaceable", "torchmaster:invisible_light")
 })
