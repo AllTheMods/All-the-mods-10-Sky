84eb64951d34cecea9723181b4b23b393d60218f
ltdk <usr@ltdk.xyz>
2025-12-11 16:04:18 -0500

    Add compacting recipes for tiny coal, ender pearl fragments

--- kubejs/server_scripts/mods/functionalstorage/recipes.js
+++ kubejs/server_scripts/mods/functionalstorage/recipes.js
@@ -18,6 +18,11 @@ ServerEvents.recipes((allthemods) => {
       .id(`allthemods:functionalstorage/compacting/${lower.id.split(":")[1]}_to_${higher.id.split(":")[1]}`)
   }
 
+  compact(Item.of("utilitarian:tiny_charcoal", 8), Item.of("minecraft:charcoal"))
+  compact(Item.of("utilitarian:tiny_coal", 8), Item.of("minecraft:coal"))
+
+  compact(Item.of("forbidden_arcanus:ender_pearl_fragment", 4), Item.of("minecraft:ender_pearl"))
+
   compact(Item.of("exdeorum:stone_pebble", 4), Item.of("minecraft:cobblestone"))
   compact(Item.of("exdeorum:diorite_pebble", 4), Item.of("minecraft:diorite"))
   compact(Item.of("exdeorum:andesite_pebble", 4), Item.of("minecraft:andesite"))
